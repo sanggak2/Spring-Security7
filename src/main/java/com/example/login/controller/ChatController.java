2cbc2a63b181259fb50f841aa88043ba30deee2a
bong9y <opsangjohnson12@naver.com>
2026-02-06 15:30:52 +0900

    feat. 챗봇 swagger수정

--- src/main/java/com/example/login/controller/ChatController.java
+++ src/main/java/com/example/login/controller/ChatController.java
@@ -2,9 +2,12 @@ package com.example.login.controller;
 
 import com.example.login.dto.ChatDto; // 방금 만든 DTO 임포트
 import com.example.login.service.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Chat Api Controller", description = "챗봇 api컨트롤러입니다")
 @RestController
 @RequestMapping("/api/chat")
 public class ChatController {
@@ -12,6 +15,7 @@ public class ChatController {
     @Autowired
     private ChatService chatService;
 
+    @Operation(summary = "챗봇api", description = "챗봇api입니다")
     @PostMapping
     public String chat(@RequestBody ChatDto request) {
         return chatService.getChatResponse(request.getMessage());
--- src/main/java/com/example/login/controller/ChatPageController.java
+++ src/main/java/com/example/login/controller/ChatPageController.java
@@ -1,11 +1,15 @@
 package com.example.login.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.stereotype.Controller; // 👈 RestController 아님!
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Tag(name = "Chat View Controller", description = "챗봇 컨트롤러입니다")
 @Controller // 👈 "난 화면(HTML)을 보여줄 거야!"
 public class ChatPageController {
 
+    @Operation(summary = "챗봇", description = "챗봇입니다")
     @GetMapping("/chat") // 주소창에 localhost:8080/chat 입력 시
     public String chatPage() {
         return "chat"; // templates/chat.mustache 파일을 찾아감
